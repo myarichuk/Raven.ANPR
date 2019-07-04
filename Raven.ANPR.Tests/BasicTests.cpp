ebe9eeb3e720fe64010b8f5aef7853c2e555a0c2
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-07-04 08:32:50 +0300

    tests finally properly wired...

---
+++ Raven.ANPR.Tests/BasicTests.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE BasicTests
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(BasicTests)
+
+BOOST_AUTO_TEST_CASE( Test1 )
+{		
+    BOOST_CHECK_EQUAL(1,1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
