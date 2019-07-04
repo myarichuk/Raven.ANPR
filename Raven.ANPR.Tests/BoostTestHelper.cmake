d699d2cd672b8877a23a336363e79f96fb72c41e
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-07-04 08:47:56 +0300

    test wiring works

--- Raven.ANPR.Tests/BoostTestHelper.cmake
+++ Raven.ANPR.Tests/BoostTestHelper.cmake
@@ -12,31 +12,19 @@ function(add_boost_test SOURCE_FILE_NAME DEPENDENCY_LIB)
 
 	file(READ "${SOURCE_FILE_NAME}" SOURCE_FILE_CONTENTS)
 
-	string(REGEX MATCH "BOOST_FIXTURE_TEST_SUITE\\( *([A-Za-z_0-9]+), [A-Za-z_0-9]+ *\\)"
-		   SUITE_MATCHES ${SOURCE_FILE_CONTENTS})
+	#first, register all auto tests
+    string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)" 
+           FOUND_TESTS ${SOURCE_FILE_CONTENTS})
 
-   list(LENGTH SUITE_MATCHES N)
-   if(${N} EQUAL 1)
-	   list(GET SUITE_MATCHES 0 SUITE_MATCH)
-	   string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+), [A-Za-z_0-9]+ *\\)" "\\1" SUITE_NAME ${SUITE_MATCH})
-   else()
-	   string(REGEX MATCH "BOOST_AUTO_TEST_SUITE\\( *([A-Za-z_0-9]+) *\\)"
-			  SUITE_MATCHES ${SOURCE_FILE_CONTENTS})
-	   list(LENGTH SUITE_MATCHES N)
-	   if(${N} EQUAL 1)
-		   list(GET SUITE_MATCHES 0 SUITE_MATCH)
-		   string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\)" "\\1" SUITE_NAME ${SUITE_MATCH})
-	   endif()
-   endif()
+	message("Discovering tests:")
+    foreach(HIT ${FOUND_TESTS})
+        string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" TEST_NAME ${HIT})
+		
+		message("Found ${TEST_NAME}")
 
-	string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)" 
-		FOUND_AUTO_TESTS ${SOURCE_FILE_CONTENTS})
-
-	foreach(HIT ${FOUND_AUTO_TESTS})
-		string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" TEST_NAME ${HIT})
-
-		add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}" 
-				 COMMAND ${TEST_EXECUTABLE_NAME}
-				 --run_test=${SUITE_NAME}/${TEST_NAME} --catch_system_error=yes)
-	endforeach()
+        add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}" 
+                 COMMAND ${TEST_EXECUTABLE_NAME}
+                 --run_test=${TEST_NAME} --catch_system_error=yes)
+    endforeach()
+	message("Done with discovering tests...")
 endfunction()
